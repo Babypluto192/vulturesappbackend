20f15dcb29ca0cd0e404c2082f62b735dba46134
Babypluto192 <saparuslan111105@gmail.com>
2024-06-02 23:04:17 +0500

    0.0.4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const unzipper = require('unzipper');
+const News = require('./News')
 
 const app = express();
 app.use(express.json());
@@ -21,9 +22,49 @@ app.listen(PORT, () => {
     console.log("Server started on " + PORT);
 });
 
-app.get('/tracks', async (req, res) => {
+app.get('/vultures1', async (req, res) => {
     const zipPath = path.join(__dirname, 'public', 'Vultures.zip');
-    const extractPath = path.join(__dirname, 'public', 'tracks');
+    const extractPath = path.join(__dirname, 'public', 'vultures1');
+
+
+    if (!fs.existsSync(extractPath)) {
+        fs.mkdirSync(extractPath, { recursive: true });
+    }
+
+
+    fs.createReadStream(zipPath)
+        .pipe(unzipper.Extract({ path: extractPath }))
+        .on('close', () => {
+            console.log('Extraction complete. Files:');
+            fs.readdir(extractPath, (err, files) => {
+                if (err) {
+                    console.error('Failed to read extracted files', err);
+                    return res.status(500).json({ error: 'Failed to read extracted files' });
+                }
+                files.sort((a, b) => {
+                    const numA = parseInt(a.match(/\d+/), 10);
+                    const numB = parseInt(b.match(/\d+/), 10);
+                    return numA - numB;
+                });
+                files.forEach(file => console.log(file));
+                const fileUrls = files.map(file => ({
+                    fileName: file,
+                    fileURL: `/vultures1/${file}`
+                }));
+                res.json(fileUrls);
+            });
+        })
+        .on('error', (err) => {
+            console.error('Failed to unzip the file', err);
+            res.status(500).json({ error: 'Failed to unzip the file' });
+        });
+});
+
+app.use('/vultures1', express.static(path.join(__dirname, 'public', 'vultures1')));
+
+app.get('/prevultures', async (req, res) => {
+    const zipPath = path.join(__dirname, 'public', 'Leaks.zip');
+    const extractPath = path.join(__dirname, 'public', 'prevultures');
 
 
     if (!fs.existsSync(extractPath)) {
@@ -43,7 +84,7 @@ app.get('/tracks', async (req, res) => {
                 files.forEach(file => console.log(file));
                 const fileUrls = files.map(file => ({
                     fileName: file,
-                    fileURL: `/tracks/${file}`
+                    fileURL: `/prevultures/${file}`
                 }));
                 res.json(fileUrls);
             });
@@ -54,4 +95,82 @@ app.get('/tracks', async (req, res) => {
         });
 });
 
-app.use('/tracks', express.static(path.join(__dirname, 'public', 'tracks')));
+app.use('/prevultures', express.static(path.join(__dirname, 'public', 'prevultures')));
+
+
+app.get('/badbook', async (req, res) => {
+    const zipPath = path.join(__dirname, 'public', 'BadBook.zip');
+    const extractPath = path.join(__dirname, 'public', 'badbook');
+
+
+    if (!fs.existsSync(extractPath)) {
+        fs.mkdirSync(extractPath, { recursive: true });
+    }
+
+
+    fs.createReadStream(zipPath)
+        .pipe(unzipper.Extract({ path: extractPath }))
+        .on('close', () => {
+            console.log('Extraction complete. Files:');
+            fs.readdir(extractPath, (err, files) => {
+                if (err) {
+                    console.error('Failed to read extracted files', err);
+                    return res.status(500).json({ error: 'Failed to read extracted files' });
+                }
+                files.forEach(file => console.log(file));
+                const fileUrls = files.map(file => ({
+                    fileName: file,
+                    fileURL: `/badbook/${file}`
+                }));
+                res.json(fileUrls);
+            });
+        })
+        .on('error', (err) => {
+            console.error('Failed to unzip the file', err);
+            res.status(500).json({ error: 'Failed to unzip the file' });
+        });
+});
+
+app.use('/badbook', express.static(path.join(__dirname, 'public', 'badbook')));
+
+app.get('/vultures2', async (req, res) => {
+    const zipPath = path.join(__dirname, 'public', 'Vultures2.zip');
+    const extractPath = path.join(__dirname, 'public', 'vultures2');
+
+
+    if (!fs.existsSync(extractPath)) {
+        fs.mkdirSync(extractPath, { recursive: true });
+    }
+
+
+    fs.createReadStream(zipPath)
+        .pipe(unzipper.Extract({ path: extractPath }))
+        .on('close', () => {
+            console.log('Extraction complete. Files:');
+            fs.readdir(extractPath, (err, files) => {
+                if (err) {
+                    console.error('Failed to read extracted files', err);
+                    return res.status(500).json({ error: 'Failed to read extracted files' });
+                }
+                files.forEach(file => console.log(file));
+                const fileUrls = files.map(file => ({
+                    fileName: file,
+                    fileURL: `/vultures2/${file}`
+                }));
+                res.json(fileUrls);
+            });
+        })
+        .on('error', (err) => {
+            console.error('Failed to unzip the file', err);
+            res.status(500).json({ error: 'Failed to unzip the file' });
+        });
+});
+
+app.use('/vultures2', express.static(path.join(__dirname, 'public', 'vultures2')));
+
+
+app.get('/news', (req, res) => {
+    res.send(News)
+});
+
+app.use('/images',express.static(path.join(__dirname, 'public', 'images')))
